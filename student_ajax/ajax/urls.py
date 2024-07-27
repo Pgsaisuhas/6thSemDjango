13f6f659cd964a8aac179973f14cd9be1f526a38
Pgsaisuhas <saisuhas.1324@gmail.com>
2024-07-27 08:31:31 +0530

    exp 12 and 13 combine

--- student_ajax/ajax/urls.py
+++ student_ajax/ajax/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('register/', views.registration, name="regitration"),
-    path('get_enrollments/', views.get_enrollments, name="enrollments")
+    path('get_enrollments/', views.get_enrollments, name="enrollments"),
+    path('course-search/', views.course_search, name="course-search")
 ]
--- student_ajax/ajax/views.py
+++ student_ajax/ajax/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.http import JsonResponse
+from django.http import HttpResponse, JsonResponse
+from django.template.loader import render_to_string
 from django.views.decorators.csrf import csrf_exempt
 from .models import Course, Student
 
@@ -8,7 +9,6 @@ def registration(request):
     if request.method == 'POST':
         student_id = request.POST.get('s_name')
         course_id = request.POST.get('c_name')
-
         try:
             student = Student.objects.get(id=student_id)
             course = Course.objects.get(id=course_id)
@@ -20,7 +20,12 @@ def registration(request):
             return JsonResponse({"message": "Student Already Enrolled!"}, status=200)
         
         student.enrollment.add(course)
-        return JsonResponse({"message": "Student Enrolled Successfully!"}, status=200)
+        table_html = get_enrollments()
+        
+        return JsonResponse({
+            "message": "Student Enrolled Successfully!",
+            "tableHtml": table_html
+        }, status=200)
     else:
         students = Student.objects.all()
         courses = Course.objects.all()
@@ -36,4 +41,21 @@ def get_enrollments(request):
             "student_name": student.student_name,
             "courses": ", ".join(courses)
         })
-    return JsonResponse({"enrollments": enrollments})
+    return render_to_string('ajax/student_table.html', {"enrollments": enrollments})
+
+def course_search(request):
+    if request.method == 'POST':
+        course_id = request.POST.get('c_name')
+        
+        # Filter students by course_id
+        student_list = Student.objects.filter(enrollment__id=course_id)
+        
+        if not student_list.exists():
+            return HttpResponse("<h1>No Student Enrolled</h1>")
+        
+        return render(request, 'ajax/student_list.html', context={"student_list": student_list})
+    
+    else:
+        courses = Course.objects.all()
+        return render(request, 'ajax/course_search.html', {"courses": courses})
+        
